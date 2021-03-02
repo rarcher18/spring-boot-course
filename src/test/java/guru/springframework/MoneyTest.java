8682c59c45b10ff5daf907ddeb10e9f5a9aa699e
Ryan Archer <ryanarcheryeah@gmail.com>
2021-03-01 21:28:23 -0500

    Added another test on Reducing the overall amount of money. Lesson 28: Make It

--- src/test/java/guru/springframework/MoneyTest.java
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -58,4 +58,11 @@ public class MoneyTest {
         Money result = bank.reduce(Money.dollar(1), "USD");
         assertEquals(Money.dollar(7), result);
     }
+
+    @Test
+    void testReduceMoney() {
+        Bank bank = new Bank();
+        Money result = bank.reduce(Money.dollar(1), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
 }
